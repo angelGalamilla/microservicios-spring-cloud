c53a060c2ba7db9fd06586db3aadcb80c66735bd
Angeliux <angel.g.alamilla@gmail.com>
2020-05-21 16:31:44 -0500

    Consulta por Alumno-Examen-Respuestas

--- microservicios-respuestas/src/main/java/com/aga/mx/microservicios/app/respuestas/models/repository/RespuestaRepository.java
+++ microservicios-respuestas/src/main/java/com/aga/mx/microservicios/app/respuestas/models/repository/RespuestaRepository.java
@@ -1,9 +1,13 @@
 package com.aga.mx.microservicios.app.respuestas.models.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.aga.mx.microservicios.app.respuestas.models.entity.Respuesta;
 
 public interface RespuestaRepository extends CrudRepository<Respuesta, Long> {
 
+	@Query("select r from Respuesta r join fetch r.alumno a join fetch r.pregunta p join fetch p.examen e where a.id = ? and e.id = ?")
+	public Iterable<Respuesta> findRespuestaByAlumnoByExamen(Long alumnoId, Long examenId);
+	
 }
--- microservicios-respuestas/src/main/java/com/aga/mx/microservicios/app/respuestas/services/RespuestaService.java
+++ microservicios-respuestas/src/main/java/com/aga/mx/microservicios/app/respuestas/services/RespuestaService.java
@@ -4,4 +4,5 @@ import com.aga.mx.microservicios.app.respuestas.models.entity.Respuesta;
 
 public interface RespuestaService {
 	public Iterable<Respuesta> saveAll(Iterable<Respuesta> respuestas);
+	public Iterable<Respuesta> findRespuestaByAlumnoByExamen(Long alumnoId, Long examenId);
 }
